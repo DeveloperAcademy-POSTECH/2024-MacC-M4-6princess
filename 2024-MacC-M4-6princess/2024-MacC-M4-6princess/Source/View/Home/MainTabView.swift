5670218c0943f926a3213918fc9765b86381d688
darongzzang <101727090+darongzzang@users.noreply.github.com>
2025-03-03 20:56:55 +0900

    Fix: #143 - 탭바 요소 간격 조정

--- 2024-MacC-M4-6princess/2024-MacC-M4-6princess/Source/View/Home/MainTabView.swift
+++ 2024-MacC-M4-6princess/2024-MacC-M4-6princess/Source/View/Home/MainTabView.swift
@@ -7,72 +7,6 @@
 
 import SwiftUI
 
-//struct MainTabView: View {
-//    @EnvironmentObject private var naviManager: NavigationManager
-//    @EnvironmentObject private var frameManager: FrameManager
-//    @Environment(\.managedObjectContext) private var viewContext
-//    @State private var selectedTab = 1
-//
-//    var body: some View {
-//        ZStack(alignment: .bottom) {
-//            TabView(selection: $selectedTab) {
-//                HomeView()
-//                    .tabItem {
-//                        VStack {
-//                            if selectedTab == 0 {
-//                                Image("homeIconFill")
-//                                    .frame(width: 24, height: 24)
-//                            } else {
-//                                Image("homeIcon")
-//                                    .frame(width: 24, height: 24)
-//
-//                            }
-//                            Text("홈")
-//                        }
-//                        .padding(.top, 13)
-//
-//
-//                    }
-//                    .tag(0)
-//                    .onAppear { selectedTab = 0 }
-//
-//                CameraView()
-//                    .environmentObject(frameManager)
-//                    .environmentObject(naviManager)
-//                    .tabItem {
-//                        VStack{
-//                            if selectedTab == 1 {
-//                                Image("cameraIconFill")
-//                                    .frame(width: 24, height: 24)
-//                            } else {
-//                                Image("cameraIcon")
-//                                    .frame(width: 24, height: 24)
-//                            }
-//                            Text("카메라")
-//                        }
-//                        .padding(.top, 13)
-//                    }
-//                    .tag(1)
-//                    .onAppear { selectedTab = 1 }
-//            }
-//            .tint(Color.gray01)
-//            .environmentObject(frameManager)
-//            .environmentObject(naviManager)
-//
-//            // 탭바 위쪽에 선 추가
-//            Rectangle()
-//                .fill(Color.gray10)
-//                .frame(height: 0.5)
-//                .edgesIgnoringSafeArea(.horizontal)
-//                .padding(.bottom, 56) // safeArea 고려해서 76->56으로 조정
-//        }
-//    }
-//}
-//
-//#Preview {
-//    MainTabView()
-//}
-
 struct MainTabView: View {
     @EnvironmentObject private var naviManager: NavigationManager
     @EnvironmentObject private var frameManager: FrameManager
@@ -85,15 +19,14 @@ struct MainTabView: View {
             // 선택된 탭에 따라 뷰 전환
             if selectedTab == 0 {
                 HomeView()
-                    .padding(.bottom, tabBarHeight) // 탭바 높이만큼 올리기
+                    .padding(.bottom, tabBarHeight)
             } else {
                 CameraView()
                     .environmentObject(frameManager)
                     .environmentObject(naviManager)
-                    .padding(.bottom, tabBarHeight) // 탭바 높이만큼 올리기
+                    .padding(.bottom, tabBarHeight)
             }
             
-            // 커스텀 탭 바
             CustomTabBar(selectedTab: $selectedTab)
         }
         .environmentObject(frameManager)
@@ -108,13 +41,13 @@ struct CustomTabBar: View {
         
         VStack {
             HStack {
-                Spacer()
                 
                 TabBarButton(imageName: selectedTab == 0 ? "homeIconFill" : "homeIcon",
                              title: "홈",
                              isSelected: selectedTab == 0) {
                     selectedTab = 0
                 }
+                             .padding(.leading, 80)
                 
                 Spacer()
                 
@@ -123,8 +56,8 @@ struct CustomTabBar: View {
                              isSelected: selectedTab == 1) {
                     selectedTab = 1
                 }
+                             .padding(.trailing, 80)
                 
-                Spacer()
             }
             .padding(.top, 13)
             .background(
