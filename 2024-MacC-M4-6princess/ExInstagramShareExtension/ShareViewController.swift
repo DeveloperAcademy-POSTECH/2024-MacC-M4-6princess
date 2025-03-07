5fc092dcf582443f98bf583892d1913a7f13e67d
piri <pyoram25@gmail.com>
2025-03-07 18:38:35 +0900

    Feat: #141 - 쉐어 확장 설치

---
+++ 2024-MacC-M4-6princess/ExInstagramShareExtension/ShareViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ShareViewController.swift
+//  ExInstagramShareExtension
+//
+//  Created by ram on 3/7/25.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
+//let defaults = UserDefaults(suiteName: "group.ex.instagram")
+// Instagram 전용 확장 (선택 사항)
+extension UIActivity.ActivityType {
+    static let postToInstagram = UIActivity.ActivityType(rawValue: "com.instagram.share")
+}
+class ShareViewController: UIViewController {
+    private let label = {
+        let label = UILabel()
+        label.text = "test"
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray.withAlphaComponent(0.8)
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+}
